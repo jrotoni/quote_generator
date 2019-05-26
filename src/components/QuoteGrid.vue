6751b782d66786108370717c5c4294a69233ac5b
Jeremy Rotoni <jeremy.rotoni@gmail.com>
2019-05-27 05:24:19 +0800

    Quote generator using Vue.js

---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="row">
+    <app-quote v-for="(quote,i) in quotes" :key="i" @click.native="deleteQuote(i)">
+      {{quote}}
+    </app-quote>
+  </div>
+</template>
+
+<script>
+import Quote from './Quote.vue';
+
+export default {
+  props: ['quotes'],
+  components: {
+    appQuote: Quote
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    deleteQuote(i) {
+      this.$emit('quoteDeleted', i);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
